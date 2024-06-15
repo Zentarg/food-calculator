0826f5943655b6142f1cf9a44cf4d41ad68c38f3
Zentarg <apg.chili@gmail.com>
2024-06-15 13:19:14 +0200

    Add base routes and fix css issues

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { DiaryComponent } from './features/diary/diary.component';
+import { SettingsComponent } from './features/settings/settings.component';
+import { StatisticsComponent } from './features/statistics/statistics.component';
 
-export const ROUTES: Routes = [];
+export const ROUTES: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'diary'
+    },
+    {
+        path: 'diary',
+        pathMatch: 'full',
+        component: DiaryComponent
+    },
+    {
+        path: 'settings',
+        pathMatch: 'full',
+        component: SettingsComponent
+    },
+    {
+        path: 'statistics',
+        pathMatch: 'full',
+        component: StatisticsComponent
+    }
+];
